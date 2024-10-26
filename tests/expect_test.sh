744b2522243fe5a20326b9e73275535a6b87296d
arthurlch <lachat.arthur74@gmail.com>
2024-10-26 16:26:37 +0900

    fix: build-all, update test

--- tests/expect_test.sh
+++ tests/expect_test.sh
@@ -1,6 +1,27 @@
 #!/usr/bin/expect -f
 
-spawn ./bin/cub
+set binary_path "./bin/cub_linux-amd64"  ;# default for Linux
+
+if {[string equal $::env.GITHUB_ACTIONS "true"]} {
+    if {[string match {*darwin*} $::env.RUNNER_OS]} {
+        set binary_path "./bin/cub_darwin-amd64"
+    } elseif {[string match {*Windows*} $::env.RUNNER_OS]} {
+        set binary_path "./bin/cub_windows-amd64"
+    }
+} else {
+    if {[info exists ::env.GOOS] && [string equal $::env.GOOS "darwin"]} {
+        set binary_path "./bin/cub_darwin-amd64"
+    } elseif {[info exists ::env.GOOS] && [string equal $::env.GOOS "windows"]} {
+        set binary_path "./bin/cub_windows-amd64"
+    }
+}
+
+if {![file exists $binary_path]} {
+    puts "Error: $binary_path not found!"
+    exit 1
+}
+
+spawn $binary_path
 
 send "i"                      ;# Switch to insert mode
 send "Hello, World!"           ;# Type "Hello, World!"
@@ -13,12 +34,12 @@ send "\033[B"                  ;# Move down (arrow key down)
 send "\033[D"                  ;# Move left (arrow key left)
 send "\033[C"                  ;# Move right (arrow key right)
 
-send "^S"                      ;# Press Ctrl+S to save
+send "\x13"                    ;# Press Ctrl+S to save
 
 send "dd"                      ;# Delete the current line (dd)
 
-send "^U"                      ;# Press Ctrl+U to undo the deletion
-send "^R"                      ;# Press Ctrl+R to redo the deletion
+send "\x15"                    ;# Press Ctrl+U to undo the deletion
+send "\x12"                    ;# Press Ctrl+R to redo the deletion
 
 send "s"                       ;# Start selection
 send "\033[C\033[C\033[C"      ;# Move right (selecting "Hello")
@@ -33,6 +54,6 @@ send "\033OF"                  ;# End key (move to end of line)
 send "\033[5~"                 ;# PgUp
 send "\033[6~"                 ;# PgDn
 
-send "^Q"                      ;# Press Ctrl+Q to quit
+send "\x11"                    ;# Press Ctrl+Q to quit
 
 expect eof
