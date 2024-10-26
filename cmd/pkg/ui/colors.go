8554cf65055de4f8d25ccd8d70271c0689dde677
arthurlch <lachat.arthur74@gmail.com>
2024-10-26 15:39:38 +0900

    chores: fix theme color

--- cmd/pkg/ui/colors.go
+++ cmd/pkg/ui/colors.go
@@ -5,41 +5,39 @@ import (
 )
 
 var (
-	SoftBlack     = termbox.RGBToAttribute(30, 30, 30)     
-	White         = termbox.RGBToAttribute(240, 240, 240)  
-	LightGray     = termbox.RGBToAttribute(180, 180, 180)  
-	Magenta       = termbox.RGBToAttribute(255, 85, 255)   
-	Green         = termbox.RGBToAttribute(120, 255, 120)  
-	Yellow        = termbox.RGBToAttribute(255, 215, 0)    
-	Red           = termbox.RGBToAttribute(255, 70, 70)    
-	Cyan          = termbox.RGBToAttribute(0, 255, 255)    
-	Blue          = termbox.RGBToAttribute(70, 130, 180)   
-	PinkBold      = termbox.RGBToAttribute(255, 105, 180)  
-	ColorDarkPink = termbox.RGBToAttribute(231, 84, 128)  
-	GoBlue        = termbox.RGBToAttribute(66, 165, 245)
-	
+    SoftBlack     = termbox.RGBToAttribute(1, 22, 39)      // #011627 Background
+    White         = termbox.RGBToAttribute(214, 222, 235)   // #D6DEEB Foreground
+    LightGray     = termbox.RGBToAttribute(99, 119, 119)    // #637777 Comment
+    Magenta       = termbox.RGBToAttribute(199, 146, 234)   // #C792EA Keywords
+    Green         = termbox.RGBToAttribute(173, 219, 103)   // #ADDB67 Support
+    Yellow        = termbox.RGBToAttribute(247, 140, 108)   // #F78C6C Numbers
+    Red           = termbox.RGBToAttribute(255, 99, 99)     // #FF6363 Constant
+    Cyan          = termbox.RGBToAttribute(127, 219, 202)   // #7FDBCA Language variables
+    Blue          = termbox.RGBToAttribute(130, 170, 255)   // #82AAFF Functions
+    PinkBold      = termbox.RGBToAttribute(236, 196, 141)   // #ECC48D Quoted strings
+    ColorDarkPink = termbox.RGBToAttribute(29, 59, 83)      // #1D3B53 Active selection
+    GoBlue        = termbox.RGBToAttribute(128, 164, 194)   // #80A4C2 Cursor
 )
 
-
 var (
-	ColorBackground     = SoftBlack
-	ColorForeground     = White
-	ColorHighlight      = Magenta
-	StatusBarForeground = White
-	StatusBarBackground = ColorDarkPink
-	CursorForeground    = SoftBlack
-	CursorBackground    = Green
-	TextForeground      = White  
-	TextBackground      = SoftBlack
-	ModalTextColor      = GoBlue
+    ColorBackground     = SoftBlack
+    ColorForeground     = White
+    ColorHighlight      = Magenta
+    StatusBarForeground = White
+    StatusBarBackground = ColorDarkPink
+    CursorForeground    = SoftBlack
+    CursorBackground    = Green
+    TextForeground      = White
+    TextBackground      = SoftBlack
+    ModalTextColor      = GoBlue
 
-	SelectedBackground = termbox.ColorWhite
-	SelectedForeground = termbox.ColorWhite 
+    SelectedBackground = termbox.ColorWhite
+    SelectedForeground = termbox.ColorWhite
 
-	ColorKeyword  = PinkBold
-	ColorString   = Green
-	ColorComment  = LightGray
-	ColorNumber   = Yellow
-	ColorOperator = Red
-	ColorDefault  = White
+    ColorKeyword  = PinkBold
+    ColorString   = Green
+    ColorComment  = LightGray
+    ColorNumber   = Yellow
+    ColorOperator = Red
+    ColorDefault  = White
 )
