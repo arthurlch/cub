c41f80651ff7529af1fe7e27695e97f9b6811d14
arthurlch <lachat.arthur74@gmail.com>
2024-11-03 18:34:53 +0900

    fixes: CI expect tests, imperove expect tests

--- tests/expect_test.sh
+++ tests/expect_test.sh
@@ -13,7 +13,6 @@ if {[info exists env(RUNNER_OS)]} {
 }
 
 set binary_path "./bin/cub_linux-amd64"  ;# default for Linux
-
 if {$github_actions eq "true"} {
     if {[string match {*darwin*} $runner_os]} {
         set binary_path "./bin/cub_darwin-amd64"
@@ -35,36 +34,38 @@ if {![file exists $binary_path]} {
 
 spawn $binary_path
 
-send "i"                      ;# Switch to insert mode
-send "Hello, World!"          ;# Type "Hello, World!"
+send "i"                      ;# Enter insert mode
+send "Hello, Cub!"            ;# Type "Hello, Cub!"
 send "\r"                     ;# Insert new line
-send "This is a test file.\r" ;# Type on a new line
+send "Testing basic insertions.\r" ;# Another line
 send "\033"                   ;# Exit insert mode (press Esc)
 
-send "\033\[A"                 ;# Move up (arrow key up)
-send "\033\[B"                 ;# Move down (arrow key down)
-send "\033\[D"                 ;# Move left (arrow key left)
-send "\033\[C"                 ;# Move right (arrow key right)
+send "\033\[A"                ;# Arrow key up
+send "\033\[B"                ;# Arrow key down
+send "\033\[D"                ;# Arrow key left
+send "\033\[C"                ;# Arrow key right
 
 send "\x13"                   ;# Press Ctrl+S to save
 
 send "dd"                     ;# Delete the current line (dd)
-
-send "\x15"                   ;# Press Ctrl+U to undo the deletion
-send "\x12"                   ;# Press Ctrl+R to redo the deletion
+send "\x15"                   ;# Ctrl+U to undo deletion
+send "\x12"                   ;# Ctrl+R to redo deletion
 
 send "s"                      ;# Start selection
-send "\033\[C\033\[C\033\[C"   ;# Move right (selecting "Hello")
+send "\033\[C\033\[C\033\[C"  ;# Move right to select "Cub"
 send "z"                      ;# End selection
-send "c"                      ;# Copy selection to clipboard
-send "\033\[C\033\[C"          ;# Move cursor right (move to the next word)
-send "v"                      ;# Paste from clipboard
+send "c"                      ;# Copy selection
+send "\033\[C\033\[C"         ;# Move right
+send "v"                      ;# Paste clipboard content
 
-send "\033OH"                 ;# Home key (move to beginning of line)
-send "\033OF"                 ;# End key (move to end of line)
+send "\033OH"                 ;# Home key
+send "\033OF"                 ;# End key
 
-send "\033\[5~"                ;# PgUp
-send "\033\[6~"                ;# PgDn
+send "\033\[5~"               ;# Page Up
+send "\033\[6~"               ;# Page Down
+
+send "a"                      ;# Start "Select All" action
+send "a"                      ;# Confirm "Select All"
 
 send "\x11"                   ;# Press Ctrl+Q to quit
 
