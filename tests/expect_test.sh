9cf7c7d244a35361ad95294241d619495269d1eb
arthurlch <lachat.arthur74@gmail.com>
2024-11-03 18:27:36 +0900

    fixes: CI expect tests

--- tests/expect_test.sh
+++ tests/expect_test.sh
@@ -41,10 +41,10 @@ send "\r"                     ;# Insert new line
 send "This is a test file.\r" ;# Type on a new line
 send "\033"                   ;# Exit insert mode (press Esc)
 
-send "\033[A"                 ;# Move up (arrow key up)
-send "\033[B"                 ;# Move down (arrow key down)
-send "\033[D"                 ;# Move left (arrow key left)
-send "\033[C"                 ;# Move right (arrow key right)
+send "\033\[A"                 ;# Move up (arrow key up)
+send "\033\[B"                 ;# Move down (arrow key down)
+send "\033\[D"                 ;# Move left (arrow key left)
+send "\033\[C"                 ;# Move right (arrow key right)
 
 send "\x13"                   ;# Press Ctrl+S to save
 
@@ -54,17 +54,17 @@ send "\x15"                   ;# Press Ctrl+U to undo the deletion
 send "\x12"                   ;# Press Ctrl+R to redo the deletion
 
 send "s"                      ;# Start selection
-send "\033[C\033[C\033[C"     ;# Move right (selecting "Hello")
+send "\033\[C\033\[C\033\[C"   ;# Move right (selecting "Hello")
 send "z"                      ;# End selection
 send "c"                      ;# Copy selection to clipboard
-send "\033[C\033[C"           ;# Move cursor right (move to the next word)
+send "\033\[C\033\[C"          ;# Move cursor right (move to the next word)
 send "v"                      ;# Paste from clipboard
 
 send "\033OH"                 ;# Home key (move to beginning of line)
 send "\033OF"                 ;# End key (move to end of line)
 
-send "\033[5~"                ;# PgUp
-send "\033[6~"                ;# PgDn
+send "\033\[5~"                ;# PgUp
+send "\033\[6~"                ;# PgDn
 
 send "\x11"                   ;# Press Ctrl+Q to quit
 
